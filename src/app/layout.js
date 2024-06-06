2aa0d9683181ff7f62964f6989a76dbbbfb76c4c
pragatmishra <21je0665@iitism.ac.in>
2024-06-06 16:41:47 +0530

    final

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,38 +3,41 @@ import "./globals.css";
 import News from "@/components/News";
 import Sidebar from "@/components/Sidebar";
 import SessionWrapper from "@/components/SessionWrapper";
+import CommentModal from "@/components/CommentModal";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "X Clone",
-  description: "A colen of X website built with Next.js and Tailwind css",
+  title: 'X Clone',
+  description: 'A clone of X website built with Next.js and Tailwind CSS',
 };
+
+export const dynamic = 'force-dynamic';
 
 export default function RootLayout({ children }) {
   return (
     <SessionWrapper>
-    <html lang="en">
-      <body className={inter.className}>
-        <div className="flex justify-between max-w-6xl mx-auto" >
-        <div className='hidden sm:inline border-r h-screen sticky top-0'>
-            <Sidebar/>
-          </div> 
-          <div className='w-2xl flex-1'>{children}</div>
+      <html lang='en'>
+        <body className={inter.className}>
+          <div className='flex justify-between max-w-6xl mx-auto'>
+            <div className='hidden sm:inline border-r h-screen sticky top-0'>
+              <Sidebar />
+            </div>
+            <div className='w-2xl flex-1'>{children}</div>
             <div className='lg:flex-col p-3 h-screen border-l hidden lg:flex w-[24rem]'>
-          <div className='sticky top-0 bg-white py-2'>
+              <div className='sticky top-0 bg-white py-2'>
                 <input
                   type='text'
                   placeholder='Search'
                   className='bg-gray-100 border border-gray-200 rounded-3xl text-sm w-full px-4 py-2'
                 ></input>
               </div>
-            <News/>
+              <News />
+            </div>
           </div>
-        </div>
-        
-      </body>
-    </html>
+          <CommentModal />
+        </body>
+      </html>
     </SessionWrapper>
   );
 }
--- src/components/SessionWrapper.jsx
+++ src/components/SessionWrapper.jsx
@@ -1,9 +1,14 @@
 'use client';
 
 import { SessionProvider } from 'next-auth/react';
+import { RecoilRoot } from 'recoil';
 // import { Children } from 'react';
 
-const SessionWrapper = ({children }) => {
-    return <SessionProvider>{children}</SessionProvider>;
-};
+const SessionWrapper = ({ children }) => {
+    return (
+      <SessionProvider>
+        <RecoilRoot>{children}</RecoilRoot>
+      </SessionProvider>
+    );
+  };
 export default SessionWrapper;
